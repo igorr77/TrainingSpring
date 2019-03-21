67146834e4e86ab5b184fb0e91e8001aeb42f64c
igorr77 <47474306+igorr77@users.noreply.github.com>
2019-03-21 23:58:53 +0700

    Revert "lesson05. ДЗ 4. Версия 1, 2"

--- lesson05/src/main/java/ru/otus/igorr/lesson05/services/process/InterviewProcess.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.igorr.lesson05.services.process;
-
-import ru.otus.igorr.lesson05.domain.user.User;
-
-public interface InterviewProcess {
-    User login();
-    void process();
-    double result();
-}
--- lesson05/src/main/java/ru/otus/igorr/lesson05/services/users/UserService.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.igorr.lesson05.services.users;
-
-
-import ru.otus.igorr.lesson05.domain.user.User;
-
-public interface UserService {
-    User getUser();
-}
